b084f61803a6e9068f299e43b0e98b872d56586a
katsudon16 <sucipto.kathleen@gmail.com>
2020-05-20 13:26:44 -0400

    Update readme

--- halla/run_example.py
+++ halla/run_example.py
@@ -1,7 +1,12 @@
 #TODO: move to the appropriate directory
 
 from tools import HAllA
+import yaml
+from os.path import dirname, abspath, join
+
+X_file = join(dirname(abspath(__file__)), 'data', 'X_line1_32_50.txt')
+Y_file = join(dirname(abspath(__file__)), 'data', 'Y_line1_32_50.txt')
 
 test_halla = HAllA('dummy_param')
-test_halla.load('data/X_line1_32_50.txt', 'data/Y_line1_32_50.txt')
+test_halla.load(X_file, Y_file)
 test_halla.run()
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 # HAllA revised version
 
 Note: the python setup is not yet finalized.
+
+An example code `run_example.py` is currently provided under `halla` directory.
+
+Some changes include:
+
+- the use of yaml for config, which is automatically converted into a `Struct` object by `config-loader.py`
+- refactor HAllA into a class to enable both creating the class object and calling it via a terminal command
