9956531fa8df235c975cd1611b7db2e857e97a14
katsudon16 <sucipto.kathleen@gmail.com>
2020-05-26 15:31:38 -0400

    Enable running all pdist metrics provided by scipy (including passing arguments)

--- halla/run_example.py
+++ halla/run_example.py
@@ -4,8 +4,7 @@ from tools import HAllA
 from os.path import dirname, abspath, join
 
 X_file = join(dirname(abspath(__file__)), 'data', 'X_line1_32_50.txt')
-Y_file = join(dirname(abspath(__file__)), 'data', 'Y_line1_32_50.txt')
 
-test_halla = HAllA()
-test_halla.load(X_file, Y_file)
+test_halla = HAllA(pdist_metric='minkowski', pdist_args={ 'p': 2. })
+test_halla.load(X_file)
 test_halla.run()
