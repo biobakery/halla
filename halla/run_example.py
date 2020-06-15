e554f192f6805d11a0984a247db8ad8d35364de5
katsudon16 <sucipto.kathleen@gmail.com>
2020-06-15 16:56:22 -0400

    Enable generating a full hallagram (heatmap)

--- halla/run_example.py
+++ halla/run_example.py
@@ -6,11 +6,11 @@ from os.path import dirname, abspath, join
 X_file = join(dirname(abspath(__file__)), 'data', 'X_line1_16_16.txt')
 Y_file = join(dirname(abspath(__file__)), 'data', 'Y_line1_16_16.txt')
 
-pdist_metric = 'nmi'
+pdist_metric = 'pearson'
 
 test_halla = HAllA(discretize_func='equal-freq', discretize_num_bins=4,
                   pdist_metric=pdist_metric, seed=123)
 
 test_halla.load(X_file, Y_file)
-# test_halla.run()
+test_halla.run()
 # print(test_halla.X_hierarchy.distance_matrix)
