c63111489fdb4ef231a811b2dcdcdce481281806
katsudon16 <sucipto.kathleen@gmail.com>
2020-08-04 14:56:50 -0400

    Update HMP example code

--- halla/examples/run_HMP.py
+++ halla/examples/run_HMP.py
@@ -7,11 +7,11 @@ sys.path.append(dirname(dirname(abspath(__file__))))
 from tools import HAllA
 
 dir_path = '../data/HMP'
-X_file = join(dirname(abspath(__file__)), dir_path, 'OTU_Stool_halla_data.tsv')
-Y_file = join(dirname(abspath(__file__)), dir_path, 'OTU_Stool_halla_metadata.tsv')
+X_file = join(dirname(abspath(__file__)), dir_path, 'abundance_by_species_stool.tsv')
+Y_file = join(dirname(abspath(__file__)), dir_path, 'metadata.tsv')
 
 pdist_metric = 'nmi'
 
-test_halla = HAllA(pdist_metric=pdist_metric, out_dir='local_tests/HMP_out', seed=123)
+test_halla = HAllA(pdist_metric=pdist_metric, out_dir='local_tests/HMP_out', discretize_func='equal-freq', seed=123)
 test_halla.load(X_file, Y_file, remove_na=True)
 test_halla.run()
