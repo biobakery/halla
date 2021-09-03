2eb157a72d529daf23e157e3455e7d29313e0c1f
Andrew Ghazi <andghazi@gmail.com>
2021-09-03 10:34:05 -0400

    convert examples to command line

---
+++ examples/PRISM/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+halla -x examples/PRISM/X_dataset.txt -y examples/PRISM/Y_dataset.txt -o prism_output
---
+++ examples/nutrimouse/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+halla -x examples/nutrimouse/lipid.txt -y examples/nutrimouse/gene.txt -o nutrimouse_output
